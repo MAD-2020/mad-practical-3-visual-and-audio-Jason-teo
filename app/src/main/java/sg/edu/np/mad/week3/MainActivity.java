9f492fb0650f92c8bdddf5e6f50d5e6394751cbb
Jason-teo <64177231+Jason-teo@users.noreply.github.com>
2020-05-11 22:09:46 +0800

    Updated MainActivity.java

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,40 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String VIDEO_SAMPLE = "rabbid";
+    MediaPlayer babyLaughMedia;
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVideoView = findViewById(R.id.videoView);
+        initializePlayer();
+        babyLaughMedia = MediaPlayer.create(this, R.raw.baby_laughing);
+        babyLaughMedia.start();
+
+
+    }
+    private Uri getMedia(String mediaName) {
+        return Uri.parse("android.resource://" + getPackageName() +
+                "/raw/" + mediaName);
+    }
+    private void initializePlayer() {
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(videoUri);
+        mVideoView.start();
+        releasePlayer();
+    }
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
     }
 }
